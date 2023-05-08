5d2e4f5cd21b863eb7267276df4d60338d81400c
FebeFarina <alu0101430973@ull.edu.es>
2023-05-08 19:22:22 +0000

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import express from "express";
-import "./db/mongoose.js";
-import { usuariosRouter } from "./routers/usuariosRouter.js";
-
-const app = express();
-app.use(express.json());
-app.use(usuariosRouter);
+import { app } from "./app.js";
 
 const port = process.env.PORT || 3000;
 
