31c8471c4d998d76e7ec5bd1e4a34a73673f8c68
FebeFarina <alu0101430973@ull.edu.es>
2023-05-10 14:57:48 +0000

    Usuarios

--- src/routers/gruposRouter.ts
+++ src/routers/gruposRouter.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Grupo } from "../models/grupos.js";
+import { Usuario } from "../models/usuarios.js";
 
 export const gruposRouter = express.Router();
 
@@ -91,15 +92,27 @@ gruposRouter.delete("/groups", async (req, res) => {
         "You must provide at least one of the following: grupo_id, grupo_nombre",
     });
   }
-  let filter: NameIdType;
-  if (req.query.grupo_id) {
-    filter = { grupo_id: req.query.grupo_id.toString() };
-  } else if (req.query.grupo_nombre) {
-    filter = { grupo_nombre: req.query.grupo_nombre.toString() };
-  } else {
-    filter = {};
-  }
   try {
+    let filter: NameIdType;
+    if (req.query.grupo_id) {
+      filter = { grupo_id: req.query.grupo_id.toString() };
+    } else if (req.query.grupo_nombre) {
+      filter = { grupo_nombre: req.query.grupo_nombre.toString() };
+    } else {
+      filter = {};
+    }
+
+    // Eliminar el grupo de los usuarios
+    const deleted_group = await Grupo.findOne(filter);
+    const usuarios = await Usuario.find({});
+    usuarios.forEach(async (usuario) => {
+      const index = usuario.grupos.indexOf(deleted_group?._id);
+      if (index > -1) {
+        usuario.grupos.splice(index, 1);
+      }
+      await usuario.save();
+    });
+
     const grupo = await Grupo.findOneAndDelete(filter);
     if (grupo) {
       return res.status(200).send(grupo);
